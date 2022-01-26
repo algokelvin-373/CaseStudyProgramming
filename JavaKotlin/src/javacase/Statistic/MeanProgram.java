92258d380ee54ffc37d6f2eb1d6e32d57e33b267
algokelvin-373 <kelvinht373@gmail.com>
2022-01-27 00:15:07 +0700

    [Java] Success Create Mean Program

--- JavaKotlin/src/javacase/Statistic/MeanProgram.java
+++ JavaKotlin/src/javacase/Statistic/MeanProgram.java
@@ -1,25 +1,40 @@
 package javacase.Statistic;
 
 public class MeanProgram {
+    private static int[] dataNumber;
+
     public static void main(String[] args) {
-        String data = "532, 61, 3323, 5434, 3233";
-        String removeSpace = data.replace(" ", "");
-        long countComa = removeSpace.chars().filter(c -> c == ',').count();
-        int[] dataNumber = new int[removeSpace.length() - (int) countComa];
+        String data = "5, 6, 3, 5, 3, 5, 4, 5, 7, 8, 5, 5";
+        int[] dtNum = getDataNumber(data);
 
-        // Success Make Data Set Array
-        /*int y = 0, z = 0;
-        for (int x = 0; x < removeSpace.length(); x++) {
-            if (removeSpace.charAt(x) == ',') {
-                dataNumber[y] = Integer.parseInt(removeSpace.substring(z, x));
-                y++;
-                z = x + 1;
-            }
+        int sum = 0;
+        for (int dt: dtNum) {
+            sum += dt;
         }
-        for (int d: dataNumber) {
-            System.out.print(d + " ");
-        }*/
-
+        float mean = (float) sum / dtNum.length;
+        System.out.println("Data : " + data);
+        System.out.println("n    : " + dtNum.length);
+        System.out.println("Mean : " + mean);
 
     }
+
+    private static int[] getDataNumber(String data) {
+        String s = data.replace(" ", "");
+        long countComa = s.chars().filter(c -> c == ',').count();
+        dataNumber = new int[s.length() - (int) countComa];
+
+        int y = 0;
+        for (int x = 0; x < s.length(); x++) {
+            if (s.charAt(x) == ',')
+                setDataNumberArray(y++, s.charAt(x-1));
+            if (x == s.length() - 1)
+                setDataNumberArray(y, s.charAt(x));
+        }
+        return dataNumber;
+    }
+
+    private static void setDataNumberArray(int i, char c) {
+        dataNumber[i] = Integer.parseInt(String.valueOf(c));
+    }
+
 }
