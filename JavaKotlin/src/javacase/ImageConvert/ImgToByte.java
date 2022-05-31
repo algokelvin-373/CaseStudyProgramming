8ba197488043312d6a5cb64b9e5ef65303a87fdc
algokelvin-373 <kelvinht373@gmail.com>
2022-05-31 15:41:11 +0700

    [Java] Create method and set time execute

--- JavaKotlin/src/javacase/ImageConvert/ImgToByte.java
+++ JavaKotlin/src/javacase/ImageConvert/ImgToByte.java
@@ -1,38 +1,30 @@
 package javacase.ImageConvert;
 
-import java.io.ByteArrayOutputStream;
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
-import javax.imageio.ImageIO;
 
 public class ImgToByte {
     public static void main(String[] args) throws IOException {
-        BufferedImage bImage = ImageIO.read(new File("src/javacase/ImageConvert/sample.png"));
-        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        ImageIO.write(bImage, "png", bos );
-        byte [] data = bos.toByteArray();
+        String path = "src/javacase/ImageConvert/sample.png";
 
-        String byteString = Arrays.toString(data);
-        int lengthByte = byteString.length();
-        System.out.println("ByteString: "+ byteString);
-        System.out.println("Length: "+ lengthByte);
-        System.out.println("\n");
+        long start = System.currentTimeMillis();
 
-        String hex = byteToHex(data);
+        ConvertImg imgConvert = new ConvertImg(new File(path));
+        imgConvert.convertImgToByte();
+        byte[] data = imgConvert.getData();
+        String hex = imgConvert.byteToHex(data);
         int lengthHex = hex.length();
+
+        long finish = System.currentTimeMillis();
+
+        System.out.println("ByteString: "+ imgConvert.getByteString());
+        System.out.println("\n");
         System.out.println("HexString: "+ hex);
         System.out.println("Length: "+ lengthHex);
         System.out.println("\n");
-    }
 
-    private static String byteToHex(byte[] byteArray) {
-        String hex = "";
-        for (byte i : byteArray) {
-            hex += String.format("%02X", i);
-        }
-        return hex;
+        System.out.println("Time execution: "+ (finish - start));
     }
 
 }
